4df0b10960142ba3862135d7fa3548a1a6b22242
Adriano Henrique Cantão <adriano.cantao@gmail.com>
2020-02-16 20:07:38 -0300

    Code improvement
    Updating the code improved during November/2019.

--- classifier_decision_boundary.R
+++ classifier_decision_boundary.R
@@ -5,6 +5,12 @@
 #For teaching: User manually calls the function passing the model, dataset name and its libraries
 #load or install libraries, then execute.
 
+#function to get plot ids to arrange the plotting order in grid.arrange()
+select_grobs <- function(lay) {
+  id <- unique(c(t(lay))) 
+  id[!is.na(id)]
+} 
+
 boundary <- function(models, datasetName, inducerName, k=1, setFiletype="pdf"){ 
   setFilename <- gsub("\\([\\w\\d\\s\\=\\.,]*\\)", "", datasetName, perl=TRUE, ignore.case=TRUE)
   setFilename <- paste( inducerName, ".", setFilename, ".", setFiletype, sep="")
@@ -33,7 +39,7 @@ boundary <- function(models, datasetName, inducerName, k=1, setFiletype="pdf"){
     coord_fixed(ratio = 0.8) +
     theme(axis.ticks=element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
           axis.text=element_blank(), axis.title=element_blank(), legend.position = 'none', 
-          plot.title = element_text(size = 8, face = "bold", hjust = 0.5, color = "brown") )
+          plot.title = element_text(size = 15, face = "bold", hjust = 0.5, color = "brown") )
   
   #plots dataset and decision boundary
   hs <- 0.05
@@ -48,25 +54,58 @@ boundary <- function(models, datasetName, inducerName, k=1, setFiletype="pdf"){
     model <- eval( parse( text=mod ) )
     plotTitle <- paste(inducerName, k[i])
     Z <- predict(model, grid.df, type = "class")
-    
+    if(inducerName == "Deep Learning"){
+      Z = round(Z[,2])
+    }
     plotList[[length(plotList)+1]] <- 
       ggplot()+
-      geom_tile(aes_string(x = grid[,1],y = grid[,2],fill= Z ), alpha = 0.3, show.legend = F)+ 
+      geom_tile(aes_string(x = grid[,1],y = grid[,2],fill= as.factor(Z) ), alpha = 0.3, show.legend = F)+ 
       geom_point(data = data, aes(x=x1, y=x2, color = as.character(class)), size = 1) + theme_bw(base_size = 15) +
       ggtitle( plotTitle ) +
       coord_fixed(ratio = 0.8) + 
       theme(axis.ticks=element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
             axis.text=element_blank(), axis.title=element_blank(), legend.position = 'none', 
-            plot.title = element_text(size = 8, face = "bold", hjust = 0.5, color = "brown") )
+            plot.title = element_text(size = 15, face = "bold", hjust = 0.5, color = "brown") )
   }
-  #arrange the plots side-by-side
-  grid.arrange(grobs=plotList, ncol=length(plotList))
+  #arrange the plots according to the quantity of plots
+  if(length(plotList) == 2){
+    hlay <- rbind(c(1, 2))
+  }else if(length(plotList) == 3){
+    hlay <- rbind(c(1, NA),
+                  c(2, 3))
+  }else if(length(plotList) == 4){
+    hlay <- rbind(c(1, 2),
+                  c(3, 4))
+  }else if(length(plotList) == 5){
+    hlay <- rbind(c( 1,2,3),
+                  c(NA,4,5))
+  }else if(length(plotList) == 6){
+    hlay <- rbind(c(1,2,3),
+                  c(4,5,6))
+  }else if(length(plotList) == 7){
+    hlay <- rbind(c( 1,2,3,4),
+                  c(NA,5,6,7))
+  }else if(length(plotList) == 8){
+    hlay <- rbind(c(1,2,3,4),
+                  c(5,6,7,8))
+  }else if(length(plotList) == 9){
+    hlay <- rbind(c( 1,2,3,4,5),
+                  c(NA,6,7,8,9))
+  }else if(length(plotList) == 10){
+    hlay <- rbind(c(1,2,3,4,5 ),
+                  c(6,7,8,9,10))
+  }else {
+    print("It is configured only up to 10 plots in a single image. If you need more, add it right before this message.")
+  }
+  #arrange many plots inside a single one
+  grid.arrange(grobs=plotList[select_grobs(hlay)], layout_matrix=hlay)
   #save to file
-  ggsave(filename = setFilename, 
-         plot = arrangeGrob( grobs=plotList, ncol=length(plotList)),
+  ggsave(filename = setFilename,
+         #plot = arrangeGrob( grobs=plotList, ncol=length(plotList)),
+         plot = grid.arrange(grobs=plotList[select_grobs(hlay)], layout_matrix=hlay),
          path = outputPath,
-         width = 297, 
-         height = 210, 
+         width = 297,
+         height = 210,
          units = "mm")
   #return (plotList)
 }
@@ -78,35 +117,33 @@ require(ggplot2)
 require(caret)
 require(lattice)
 require(gridExtra)
-#datasetNames <- c("mlbench.2dnormals(n=3000, cl=2)",
-#                 "mlbench.cassini(n=5000)")
 
-datasetNames <- c("mlbench.2dnormals(n=3000, cl=2)",
-                  "mlbench.cassini(n=5000)",
-                  "mlbench.hypercube(n=3000, d=2, sd=0.1)",
-                  "mlbench.circle(n=3000, d=2)",
-                  "mlbench.ringnorm(n=3000, d=2)",
-                  "mlbench.shapes(n=3000)",
-                  "mlbench.simplex(n = 1000, d = 3, sides = 1, sd = 0.1, center=TRUE)",
+datasetNames <- c("mlbench.2dnormals(n=1000, cl=2)",
+                  "mlbench.cassini(n=1000)",
+                  "mlbench.hypercube(n=1000, d=2, sd=0.1)",
+                  "mlbench.circle(n=1000, d=2)",
+                  "mlbench.ringnorm(n=1000, d=2)",
+                  "mlbench.shapes(n=1000)",
+                  "mlbench.simplex(n = 1000, d = 2, sides = 1, sd = 0.1, center=TRUE)",
                   "mlbench.smiley(n=1000, sd1 = 0.1, sd2 = 0.05)",
                   "mlbench.spirals(n=1000,cycles=1,sd=0.05)",
                   "mlbench.spirals(n=1000,cycles=3,sd=0.05)",
-                  "mlbench.threenorm(n=3000, d=2)",
+                  "mlbench.threenorm(n=1000, d=2)",
                   "mlbench.twonorm(n=1000, d=2)",
-                  "mlbench.xor(n=3000, d=2)")
+                  "mlbench.xor(n=1000, d=2)")
 
+set.seed(2019)
 for(datasetName in datasetNames){
-  #----------------------------------------------------------------
   # KNN
   require(caret)
   inducerName <- "KNN"
-  k = c(1,3,5,10)  #k[i]
+  k = c(1,3,5,10,30)  #k[i]
   models <- c( "knn3(class ~ ., data=data, k = k[1])",
                "knn3(class ~ ., data=data, k = k[2])",
                "knn3(class ~ ., data=data, k = k[3])",
-               "knn3(class ~ ., data=data, k = k[4])")
+               "knn3(class ~ ., data=data, k = k[4])",
+               "knn3(class ~ ., data=data, k = k[5])")
   boundary(models, datasetName, inducerName, k, setFiletype)
-  
   #----------------------------------------------------------------
   # SVM
   require(e1071) #cart
@@ -117,41 +154,87 @@ for(datasetName in datasetNames){
                "svm(class ~ ., data=data, kernel='radial')",
                "svm(class ~ ., data=data, kernel='sigmoid')")
   boundary(models, datasetName, inducerName, k, setFiletype)
-  
   #----------------------------------------------------------------
   #Trees
   require(C50)
   require(rpart)
   library(randomForest)
   inducerName <- "Trees"
-  k = c("C.50","CART", "Random Forest")
+  k = c("C.50","CART", "Random Forest 3 trees", "Random Forest 128 trees")
   models <- c("C5.0(class ~ ., data=data)",
               "rpart(class ~ ., data=data)",
-              "randomForest(class ~ ., data=data)")
+              "randomForest(class ~ ., data=data, ntree=5)",
+              "randomForest(class ~ ., data=data, ntree=128)")
   boundary(models, datasetName, inducerName, k, setFiletype)
-  
   #----------------------------------------------------------------
   #Naive Bayes
   require(e1071)
   inducerName <- "Naive Bayes"
   models <- "naiveBayes(class ~ ., data=data)"
   boundary(models, datasetName, inducerName, k="", setFiletype)
+  #----------------------------------------------------------------
+  # #Neural Network: [qty neurons]
+  require(nnet)
+  inducerName <- "ANN"
+  k = c("1","5","10", "50", "100")
+  models <- c("nnet(class ~ ., data=data, size = 1, maxit = 1000)",
+              "nnet(class ~ ., data=data, size = 5, maxit = 1000)",
+              "nnet(class ~ ., data=data, size = 10, maxit = 1000)",
+              "nnet(class ~ ., data=data, size = 50, maxit = 1000)",
+              "nnet(class ~ ., data=data, size = 100, maxit = 1000)")
+  boundary(models, datasetName, inducerName, k=k, setFiletype)
+  #----------------------------------------------------------------
+  #Deep Learning: [neurons on each hidden layer]
+  require(neuralnet)
+  inducerName <- "Deep Learning"
+  set.seed(2019)
+  k = c("[3]", "[3,7]", "[3,7,5]", "[3,7,5,2]")
+  models <- c("neuralnet(class ~ ., data=data, hidden = c(5))")
+              #"neuralnet(class ~ ., data=data, hidden = c(5,7))",
+              #"neuralnet(class ~ ., data=data, hidden = c(5,7,7))",
+              #"neuralnet(class ~ ., data=data, hidden = c(5,7,7,5))")
+  boundary(models, datasetName, inducerName, k=k, setFiletype)
 }
 
-#--------NOT-WORKING---------------------------------------------
-# Linear Discriminant Analysis
-require(MASS)
-inducerName <- "Linear Discriminant Analysis (LDA)"
-models <- "lda(class ~ ., data=data)"
+# #--------NOT-WORKING---------------------------------------------
+# # Linear Discriminant Analysis
+# require(MASS)
+# inducerName <- "Linear Discriminant Analysis (LDA)"
+# models <- "lda(class ~ ., data=data)"
+# 
+# boundary(models, datasetName, inducerName, k="", setFiletype)
+# grid.arrange( grobs=plotList, ncol=length(plotList))
+# 
 
-plotList = boundary(models, datasetName, inducerName, k="", setFiletype)
-grid.arrange( grobs=plotList, ncol=length(plotList))
 
-#--------NOT-WORKING---------------------------------------------
-#Neural Network
-require(nnet)
-inducerName <- "NN (1)"
-models <- "nnet(class ~ ., data=data, size = 1, maxit = 1000, trace = FALSE)"
+for (datasetName in datasetNames){
+  # #Neural Network
+  require(nnet)
+  inducerName <- "ANN: "
+  k = c("1","5","10", "50", "100")
+  models <- c("nnet(class ~ ., data=data, size = 1, maxit = 1000)",
+              "nnet(class ~ ., data=data, size = 5, maxit = 1000)",
+              "nnet(class ~ ., data=data, size = 10, maxit = 1000)",
+              "nnet(class ~ ., data=data, size = 50, maxit = 1000)",
+              "nnet(class ~ ., data=data, size = 100, maxit = 1000)")
+  set.seed(2019)
+  boundary(models, datasetName, inducerName, k=k, setFiletype)
+}
+ 
+require(neuralnet)
+inducerName <- "Deep Learning: "
+set.seed(2019)
+k = c("[3]", "[3,7]", "[3,7,5]", "[3,7,5,2]")
+models <- c("neuralnet(class ~ ., data=data, hidden = c(5))",
+            "neuralnet(class ~ ., data=data, hidden = c(5,7))",
+            "neuralnet(class ~ ., data=data, hidden = c(5,7,7))",
+            "neuralnet(class ~ ., data=data, hidden = c(5,7,7,5))")
+boundary(models, datasetName, inducerName, k=k, setFiletype)
 
-plotList = boundary(models, datasetName, inducerName, k="", setFiletype)
-grid.arrange( grobs=plotList, ncol=length(plotList))
+
+
+inducerName <- "Deep Learning"
+set.seed(2019)
+k = "[a,b]"
+models <- "neuralnet(class ~ ., data=data, hidden = c(8,7,6,5))"
+boundary(models, datasetName, inducerName, k=k, setFiletype)
